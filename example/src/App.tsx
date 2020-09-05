05db7f68f1fe85325de6166ea1510e20cb261a61
Jasmin Suljic <jasmin.suljich@gmail.com>
2020-09-06 00:35:36 +0200

    fix: fixed lint issues

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -7,40 +7,49 @@ export default function App() {
 
   React.useEffect(() => {
     fetch('https://mobile.webteh.hr/example/create-payment-session', {
-  method: 'POST',
-  headers: {
-    Accept: 'application/json',
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify({    })
-}).then(v => v.json()).then(json => {
-  return MonriAndroidIos.confirmPayment({
-    authenticityToken: '6a13d79bde8da9320e88923cb3472fb638619ccb',
-    developmentMode: true
-  }, {
-    clientSecret: json['client_secret'] as string,
-    card: {
-      pan: "4341 7920 0000 0044",
-      cvv: "123",
-      expiryMonth: 12,
-      expiryYear: 2020
-    },
-    transaction: {
-      email: 'jasmin.suljich@gmail.com',
-      orderInfo: 'React native bridge???',
-      phone: '061123213',
-      city: 'Sarajevo',
-      country: 'BA',
-      address: 'Laticka',
-      fullName: 'Jasmin Suljic',
-      zip: '71210'
-    }
-  })
-}).then(r => `${r.status}, amount = ${r.amount}, created_at = ${r.createdAt}, order_number = ${r.orderNumber}`).then(setResult).catch(e => {
-  setResult(e)
-})
-    
-    
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({}),
+    })
+      .then((v) => v.json())
+      .then((json) => {
+        return MonriAndroidIos.confirmPayment(
+          {
+            authenticityToken: '6a13d79bde8da9320e88923cb3472fb638619ccb',
+            developmentMode: true,
+          },
+          {
+            clientSecret: json.client_secret as string,
+            card: {
+              pan: '4341 7920 0000 0044',
+              cvv: '123',
+              expiryMonth: 12,
+              expiryYear: 2020,
+            },
+            transaction: {
+              email: 'jasmin.suljich@gmail.com',
+              orderInfo: 'React native bridge???',
+              phone: '061123213',
+              city: 'Sarajevo',
+              country: 'BA',
+              address: 'Laticka',
+              fullName: 'Jasmin Suljic',
+              zip: '71210',
+            },
+          }
+        );
+      })
+      .then(
+        (r) =>
+          `${r.status}, amount = ${r.amount}, created_at = ${r.createdAt}, order_number = ${r.orderNumber}`
+      )
+      .then(setResult)
+      .catch((e) => {
+        setResult(e);
+      });
   }, []);
 
   return (
--- src/index.tsx
+++ src/index.tsx
@@ -1,53 +1,56 @@
 import { NativeModules } from 'react-native';
 
 export type MonriApiOptions = {
-  authenticityToken: string,
-  developmentMode: boolean
-}
+  authenticityToken: string;
+  developmentMode: boolean;
+};
 
 export type SavedPaymentMethod = {
-  type: string,
-  data: {[id: string]: any}
-}
+  type: string;
+  data: { [id: string]: any };
+};
 
 export type PaymentResult = {
-  status: string,
-  currency: string,
-  amount: number,
-  orderNumber: string,
-  panToken?: string,
-  createdAt: string,
-  transactionType: string,
-  paymentMethod?: SavedPaymentMethod,
-  errors: [String]
-}
+  status: string;
+  currency: string;
+  amount: number;
+  orderNumber: string;
+  panToken?: string;
+  createdAt: string;
+  transactionType: string;
+  paymentMethod?: SavedPaymentMethod;
+  errors: [String];
+};
 
 export type Card = {
-  pan: string,
-  cvv: string,
-  expiryYear: number,
-  expiryMonth: number
-}
+  pan: string;
+  cvv: string;
+  expiryYear: number;
+  expiryMonth: number;
+};
 
 export type Transaction = {
-  email?: string,
-  fullName?: string,
-  address?: string,
-  phone?: string,
-  country?: string,
-  city?: string,
-  zip?: string,
-  orderInfo?: string
-}
+  email?: string;
+  fullName?: string;
+  address?: string;
+  phone?: string;
+  country?: string;
+  city?: string;
+  zip?: string;
+  orderInfo?: string;
+};
 
 export type ConfirmPaymentParams = {
-  clientSecret: string,
-  card: Card,
-  transaction: Transaction
-}
+  clientSecret: string;
+  card: Card;
+  transaction: Transaction;
+};
 
 type MonriAndroidIosType = {
-  confirmPayment(options: MonriApiOptions, params: ConfirmPaymentParams): Promise<PaymentResult>;
+  confirmPayment(
+    options: MonriApiOptions,
+    params: ConfirmPaymentParams
+  ): Promise<PaymentResult>;
 };
 
 const { MonriAndroidIos } = NativeModules;
